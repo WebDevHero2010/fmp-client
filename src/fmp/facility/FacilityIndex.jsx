ac9d442fce697bf5db7f8c61de8d81b68a3e5b6e
Marcus Elliott <webdevhero2010@gmail.com>
2021-11-01 20:06:02 -0400

    have working table need to fix bugs

--- src/fmp/facility/FacilityIndex.jsx
+++ src/fmp/facility/FacilityIndex.jsx
@@ -1,17 +1,69 @@
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import FacilityTable from "./FacilityTable";
+import FacilityEdit from "./FacilityEdit";
 
-class FacilityIndex extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>
-        <h1 className="adminDashH1">FacilityIndexPageWorking</h1>
-      </div>
-    );
-  }
-}
+const FacilityIndex = (props) => {
+  const [facility, setFacility] = useState([]);
+  const [editActive, setEditActive] = useState(false);
+  const [facilityToEdit, setFacilityToEdit] = useState({});
 
+  const fetchFacility = () => {
+    fetch(`http://localhost:3000/facility/`, {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: props.token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((facilityData) => {
+        setFacility(facilityData);
+        console.log(facilityData);
+      });
+  };
+
+  const editFacility = (facility) => {
+    setFacilityToEdit(facility);
+    console.log(facility);
+  };
+
+  const editOn = () => {
+    setEditActive(true);
+  };
+
+  const editOff = () => {
+    setEditActive(false);
+  };
+
+  useEffect(() => {
+    fetchFacility();
+  }, []);
+
+  return (
+    <Container fluid={true}>
+      <Row>
+        <Col>
+          <FacilityTable
+            facility={facility}
+            editFacility={editFacility}
+            editOn={editOn}
+            fetchFacility={fetchFacility}
+            token={props.token}
+          />
+        </Col>
+        {editActive ? (
+          <FacilityEdit
+            facility={facilityToEdit}
+            editOff={editOff}
+            token={props.token}
+            fetchFacility={fetchFacility}
+          />
+        ) : (
+          <></>
+        )}
+      </Row>
+    </Container>
+  );
+};
 export default FacilityIndex;
--- src/fmp/facility/FacilityTable.jsx
+++ src/fmp/facility/FacilityTable.jsx
@@ -1,13 +1,103 @@
-import { Component } from "react";
+import React from "react";
+import { Table, Button, Container, Row, Col } from "reactstrap";
+import FacilityCreate from "./FacilityCreate";
 
-class FacilityTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return <div></div>;
-  }
-}
+const FacilityTable = (props) => {
+  const deleteFacility = (facility) => {
+    fetch(`http://localhost:3000/facility/delete/${facility.id}`, {
+      method: "DELETE",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: props.token,
+      }),
+    }).then(() => props.fetchFacility());
+  };
+  console.log(props.facility);
+
+  const facilityMapper = () => {
+    return props.facility.map((facility, index) => {
+      return (
+        <tr key={index}>
+          <th scope="row">{facility.id}</th>
+          <td>{facility.facilityName}</td>
+          <td>{facility.address}</td>
+          <td>{facility.state}</td>
+          <td>{facility.zipcode}</td>
+          <td>{facility.phonenumber}</td>
+          <td>{facility.facilityType}</td>
+          <td>{facility.menuType}</td>
+          <td>{facility.operationStatus}</td>
+          <td>{facility.ownerEmail}</td>
+          <td>{facility.ownerFirstName}</td>
+          <td>{facility.ownerLastName}</td>
+          <td>{facility.ownerPhoneNumber}</td>
+          <td>{facility.ownerAddress}</td>
+          <td>{facility.ownerCity}</td>
+          <td>{facility.ownerState}</td>
+          <td>{facility.ownerZipcode}</td>
+          <td>
+            <Button
+              className="btn-update"
+              color="black"
+              onClick={() => {
+                props.editFacility(facility);
+                props.editOn();
+              }}
+            >
+              Update
+            </Button>
+            <Button
+              className="btn-delete"
+              color="black"
+              onClick={() => {
+                deleteFacility(facility);
+              }}
+            >
+              Delete
+            </Button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <Container>
+      <Row className="facility-padding">
+        <Col md="auto">
+          <h1 className="font-titles">Facilities</h1>
+        </Col>
+        <Col md="auto">
+          <FacilityCreate FacilityTable={FacilityTable} token={props.token} />
+        </Col>
+        <Col md="auto">{/* <ExportPDF /> */}</Col>
+        <hr />
+      </Row>
+      <Table id="facilityTable" borderless hover>
+        <thead>
+          <tr className="font-table">
+            <th>Facility#</th>
+            <th>Address</th>
+            <th>State</th>
+            <th>Zipcode</th>
+            <th>Phone#</th>
+            <th>Facility Type</th>
+            <th>Menu Type</th>
+            <th>Operation Status</th>
+            <th>Owner Email#</th>
+            <th>Owner Firstname</th>
+            <th>Owner Lastname</th>
+            <th>Owner Phone#</th>
+            <th>Owner Address</th>
+            <th>Owner City</th>
+            <th>Owner State</th>
+            <th>Owner Zipcode</th>
+          </tr>
+        </thead>
+        <tbody>{facilityMapper()}</tbody>
+      </Table>
+    </Container>
+  );
+};
 
 export default FacilityTable;
